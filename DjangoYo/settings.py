3d5fc51d70c5d2dc27a3e89d1b6b483de1bf91f4
Loshgoobi <dozoulremy@gmail.com>
2017-12-07 15:35:21 +0100

    settings database

--- DjangoYo/settings.py
+++ DjangoYo/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'DjangoYo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoyoo',
+        'USER': 'root',
+        'PASSWORD': 'azerty',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
 }
 
